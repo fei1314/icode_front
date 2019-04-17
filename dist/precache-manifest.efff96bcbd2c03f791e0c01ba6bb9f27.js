72114ecbd6c0f0e8f7953bbfc10cf39efecceef2
liangchaofei <liangchaofei@bytedance.com>
2019-04-17 13:49:29 +0800

    add

---
+++ dist/precache-manifest.efff96bcbd2c03f791e0c01ba6bb9f27.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = [
+  {
+    "revision": "747bc3be5fd0aa2105ce",
+    "url": "/p__Forms__BasicForm.async.js"
+  },
+  {
+    "revision": "263746e7f51be896b5cc",
+    "url": "/vendors.chunk.css"
+  },
+  {
+    "revision": "83b8956e01cd5c5d7449",
+    "url": "/p__Forms__models__form.js.async.js"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "7afcd8c462f830a40630",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "7afcd8c462f830a40630",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "263746e7f51be896b5cc",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "1a09d41e3e6fba473307",
+    "url": "/p__User__Login.async.js"
+  },
+  {
+    "revision": "38cde282cf63565db4fa",
+    "url": "/p__Account__Settings__Info.chunk.css"
+  },
+  {
+    "revision": "292358ed8538c9f48095",
+    "url": "/p__Account__Settings__models__appoint.js.async.js"
+  },
+  {
+    "revision": "eee32f5924a06493d40e",
+    "url": "/p__Account__Settings__models__collect.js.async.js"
+  },
+  {
+    "revision": "23fe2fd1a79aed3c3bfb",
+    "url": "/p__Account__Settings__models__coupons.js.async.js"
+  },
+  {
+    "revision": "3ad96352acc695a8b7d3",
+    "url": "/p__Account__Settings__models__geographic.js.async.js"
+  },
+  {
+    "revision": "e8e29d8c345e44c8f66b",
+    "url": "/p__Account__Settings__models__info.js.async.js"
+  },
+  {
+    "revision": "ffc902b8147a367bb1a0",
+    "url": "/p__Account__Settings__models__invitation.js.async.js"
+  },
+  {
+    "revision": "bf6af830208d641726f9",
+    "url": "/p__Account__Settings__models__shop.js.async.js"
+  },
+  {
+    "revision": "570ba73e00572e4b9fdf",
+    "url": "/p__Account__Settings__models__works.js.async.js"
+  },
+  {
+    "revision": "07a0d4f82b0af43206b9",
+    "url": "/p__Course__$id.async.js"
+  },
+  {
+    "revision": "df831ce599106a3f6d0d",
+    "url": "/p__Course__$idc.chunk.css"
+  },
+  {
+    "revision": "df831ce599106a3f6d0d",
+    "url": "/p__Course__$idc.async.js"
+  },
+  {
+    "revision": "fcea110aad7d513b017c",
+    "url": "/p__Course__BasicCourse.async.js"
+  },
+  {
+    "revision": "c91756b9ba9c908722bf",
+    "url": "/p__Course__models__course.js.async.js"
+  },
+  {
+    "revision": "86a389594572719f3081",
+    "url": "/p__Dashboard__Analysis.chunk.css"
+  },
+  {
+    "revision": "86a389594572719f3081",
+    "url": "/p__Dashboard__Analysis.async.js"
+  },
+  {
+    "revision": "0373dd7c6f54e3a0ba0e",
+    "url": "/p__Dashboard__models__activities.js.async.js"
+  },
+  {
+    "revision": "be3a26ceece52bef9ec0",
+    "url": "/p__Dashboard__models__chart.js.async.js"
+  },
+  {
+    "revision": "de62b4823a234e5c1607",
+    "url": "/p__Dashboard__models__monitor.js.async.js"
+  },
+  {
+    "revision": "747bc3be5fd0aa2105ce",
+    "url": "/p__Forms__BasicForm.chunk.css"
+  },
+  {
+    "revision": "e150598c9045e989b578",
+    "url": "/p__User__models__register.js.async.js"
+  },
+  {
+    "revision": "054096a3d8ee404b910c",
+    "url": "/p__User__RegisterResult.async.js"
+  },
+  {
+    "revision": "1a09d41e3e6fba473307",
+    "url": "/p__User__Login.chunk.css"
+  },
+  {
+    "revision": "054096a3d8ee404b910c",
+    "url": "/p__User__RegisterResult.chunk.css"
+  },
+  {
+    "revision": "82368fe2854908209380",
+    "url": "/p__User__Register.chunk.css"
+  },
+  {
+    "revision": "82368fe2854908209380",
+    "url": "/p__User__Register.async.js"
+  },
+  {
+    "revision": "386acc8261f61af932676e7a93e3ccbf",
+    "url": "/ggeditor/flow/decision.svg"
+  },
+  {
+    "revision": "38cde282cf63565db4fa",
+    "url": "/p__Account__Settings__Info.async.js"
+  },
+  {
+    "revision": "23f14d5686d7b771c5bffaa6cd1c3f05",
+    "url": "/ggeditor/koni/country.svg"
+  },
+  {
+    "revision": "20d46caf8e80d74473d9",
+    "url": "/p__404.async.js"
+  },
+  {
+    "revision": "8bab0e213ae4bfb2ef3dcdc99378610e",
+    "url": "/ggeditor/koni/bank.svg"
+  },
+  {
+    "revision": "10eefcb402e7ad84ac00",
+    "url": "/layouts__UserLayout.chunk.css"
+  },
+  {
+    "revision": "0e530af6fe2f20b65c32fcee3b608160",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c34897c103689b920309d1eeeca9ebad",
+    "url": "/ggeditor/flow/start.svg"
+  },
+  {
+    "revision": "cf9970196a0c31b86ad9",
+    "url": "/layouts__BasicLayout.chunk.css"
+  },
+  {
+    "revision": "10eefcb402e7ad84ac00",
+    "url": "/layouts__UserLayout.async.js"
+  },
+  {
+    "revision": "e72ea94ad4025edf7a44cf0f88591ee3",
+    "url": "/ggeditor/koni/person.svg"
+  },
+  {
+    "revision": "ee97857a1c4c0203bba4fc53018b7c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf9970196a0c31b86ad9",
+    "url": "/layouts__BasicLayout.async.js"
+  },
+  {
+    "revision": "5661ce8e2472d2727e8fd0390b22ba37",
+    "url": "/icons/icon-512x512.png"
+  },
+  {
+    "revision": "c23aa881b8d0e959f6c0aeb5ae7e4baa",
+    "url": "/icons/icon-192x192.png"
+  },
+  {
+    "revision": "347329d0982682b42d40c8162817a940",
+    "url": "/icons/icon-128x128.png"
+  },
+  {
+    "revision": "07dcd135056a94bf54b5525779ab4cef",
+    "url": "/ggeditor/koni/icon.svg"
+  },
+  {
+    "revision": "c3eb6822f00442e58f4fae8645276b29",
+    "url": "/ggeditor/flow/normal.svg"
+  },
+  {
+    "revision": "7ae8363380c2f7b1cc749be320f584ae",
+    "url": "/ggeditor/flow/model.svg"
+  },
+  {
+    "revision": "694c4300be571fdd4d8b6abbe14d2e3f",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "242816c677f778034e99",
+    "url": "/28.chunk.css"
+  },
+  {
+    "revision": "242816c677f778034e99",
+    "url": "/28.async.js"
+  }
+];
